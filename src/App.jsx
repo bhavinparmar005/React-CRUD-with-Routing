c748acd4b6d52b63c36e775523c8069bfd73dd51
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-02-19 10:09:42 +0530

    add HashRouteer

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import AddPost from "./pages/AddPost";
 import EditPost from "./pages/EditPost";
 import Login from "./pages/Login";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element ={<Login/>}/>
          <Route path="/Home" element ={<Home/>} />
          <Route path="/AddPost" element ={<AddPost/>} />
          <Route path="/EditPost" element ={<EditPost/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
